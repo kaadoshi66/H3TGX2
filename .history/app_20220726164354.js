d075f4e80ee2675544ee1e790fc7d7999f228a2e
kaadoshi66 <igoguilherme00@outlook.com>
2022-07-26 16:49:18 -0300

    igor 260720221023

---
+++ .history/app_20220726164354.js
@@ -0,0 +1,39 @@
+//carregando modulos
+const express = require ("express");
+const app =express();
+const handlebars = require ("express-handlebars");
+const bodyParser = require ("body-parser");
+const admin = require ("./routes/admin.js");
+var path = require('path');
+const mongoose = require ("mongoose");
+
+//configurações
+/*BODY PARSER*/
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//template engine
+app.engine('.hbs', handlebars.engine({extname:'.hbs',defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views', 'layouts')}));
+app.set('view engine', '.hbs');
+//Mongoose
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost/APP").then(() => {
+    console.log("MongoDB online . . .")
+}).catch((erro) => {
+    console.log("houve um erro ao se conectar ao servidor mongo: " + erro)
+});
+//public
+    app.use(express.static(path.join(__dirname, "public")))
+
+
+//rotas
+app.get('/', (req, res) => {
+    res.send('Rota principal')
+})   
+app.use('/admin', admin)
+
+//outros
+    const PORT = 8081;
+    app.listen(PORT, () =>{
+        console.log("Servidor Online ...")
+    });
---
+++ .history/routes/admin_20220726153113.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Categoria");
+const Categoria = mongoose.model("categorias");
+
+router.get('/', (req, res) => {
+    res.render("admin/index")
+       
+})
+
+router.get('/posts', (req, res) => {
+    res.send("pagina de posts")
+})
+
+router.get('/categorias', (req, res) => {
+    res.render("admin/categorias") 
+})
+
+router.get('/categorias/add', (req, res) =>{
+    res.render("admin/addcategorias")
+})
+
+router.post('/categorias/nova', (req, res) =>{
+     
+    var erros =[]
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug invalido"})
+    }
+    
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() =>{
+        console.log("Categoria salva com sucesso!")
+    }).catch((err) =>{
+        console.log("Erro ao salvar categoria!" + err)       
+    })
+
+})
+
+router.get("/teste", (req, res) =>{
+    res.send("isso é um teste")
+})
+
+
+
+
+module.exports = router
